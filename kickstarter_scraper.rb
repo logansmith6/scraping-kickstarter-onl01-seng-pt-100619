bb54e98917273c705e83717e9bd9ea12ba002cae
Logan Smith <logsmith06@gmail.com>
2019-12-04 00:39:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,8 @@ def create_project_hash
   # funding:  project.css("ul.project-stats li.first-funded strong").text.gsub("%","").to_i
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
 
   projects
