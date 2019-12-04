3a79029fa2fa13bbef96c35149fe8e602e8621d3
Logan Smith <logsmith06@gmail.com>
2019-12-04 00:37:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,20 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
+  projects = {}
+
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta span.location-name").text 
+  # funding:  project.css("ul.project-stats li.first-funded strong").text.gsub("%","").to_i
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
+
+  projects 
 binding.pry
 end
 create_project_hash 
